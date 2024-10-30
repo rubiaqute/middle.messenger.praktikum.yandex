38d55321f2da02c58dc105ffdc2fc8519656a7c9
rubiaqute <rubiaqute@gmail.com>
2024-10-30 23:29:33 +0300

    feat: add api tests

---
+++ mochaSetup.js
@@ -0,0 +1,7 @@
+import { JSDOM } from "jsdom";
+
+const jsdom = new JSDOM(`<body></body>`);
+
+global.window = jsdom.window;
+global.document = jsdom.window.document;
+global.FormData = jsdom.window.FormData;
